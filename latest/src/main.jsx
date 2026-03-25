b8f7e522e82962fe1825c640b274220c90a3e757
pandeyArushi9 <arushipandey835@gmail.com>
2026-03-25 18:07:56 +0545

    finalll

--- latest/src/main.jsx
+++ latest/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/REACT-/">
+    <BrowserRouter basename="/REACT/">
       <TaskProvider>
         <App />
       </TaskProvider>
